b73d2c8a6843a1ca0e4bf16745176e64cc1e8020
tuananh <ngtuananh9604@gmail.com>
2026-03-02 16:26:08 +0700

    View List Applications

--- recruitment-ui/src/Routes/AppRoutes.jsx
+++ recruitment-ui/src/Routes/AppRoutes.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import JobList from '../pages/JobList';
 import SavedJobs from '../pages/SavedJobs';
+import ApplicationList from '../Applications/ApplicationList';
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/jobs" element={<JobList />} />
       <Route path="/saved-jobs" element={<SavedJobs />} />
+      <Route path="/employer/applications" element={<ApplicationList />} />
       <Route path="*" element={<Navigate to="/jobs" replace />} />
     </Routes>
   );
